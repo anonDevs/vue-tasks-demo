9d363fa75e5a9bb75ef99ac65746c7a0eeb9db93
anonDevs <hussain.abeer2593@gmail.com>
2020-10-23 14:23:14 +0500

    password field update

--- demo-client/src/views/Login.vue
+++ demo-client/src/views/Login.vue
@@ -3,7 +3,7 @@
     <div class="mt-5">
       <form>
          <input v-model="user.username" placeholder="Username" type="text" class="block mt-2 p-2 bg-gray-200">
-          <input v-model="user.password" placeholder="Password" type="text" class="block mt-2 p-2 bg-gray-200">
+          <input v-model="user.password" placeholder="Password" type="password" class="block mt-2 p-2 bg-gray-200">
           <input @click.prevent='loginUser' class="p-2 mt-2 bg-red-500 block w-full" type="submit" value="Log In">
       </form>
      
